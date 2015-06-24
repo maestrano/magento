3c90e75bd182728e4d351f9f678b151e205f7c9d
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2015-06-25 09:26:56 +1000

    Added logs

--- app/code/local/Maestrano/Sso/Model/Observer.php
+++ app/code/local/Maestrano/Sso/Model/Observer.php
@@ -11,21 +11,20 @@ class Maestrano_Sso_Model_Observer
     {
         // Hook Maestrano
         if (Maestrano::sso()->isSsoEnabled()) {
+            Mage::log("## Maestrano_Sso_Model_Observer - SSO is enabled");
 
             // Get the meastrano session
-            $mnoSession = Mage::getSingleton('core/session')->getMnoSession();
+            $mnoSession = Mage::getSingleton('admin/session')->getMnoSession();
 
             if ($mnoSession == null)
-                echo "Session is null !!";
-            else
-                print_r($mnoSession);
+                Mage::log("## Maestrano_Sso_Model_Observer - Session is null!!");
 
             // Check session is present and valid, then trigger SSO if not
             if ($mnoSession == null || !$mnoSession->isValid()) {
+                Mage::log("## Maestrano_Sso_Model_Observer - Session is not valid.");
+
                 // The session may have bee updated while validation checking
-                if ($mnoSession != null) {
-                    Mage::getSingleton('core/session')->setMnoSession($mnoSession);
-                }
+                Mage::getSingleton('admin/session')->setMnoSession($mnoSession);
 
                 // Call the init action which will call the SSO server
                 header('Location: ' . Maestrano::sso()->getInitPath());
--- app/code/local/Maestrano/Sso/controllers/SamlController.php
+++ app/code/local/Maestrano/Sso/controllers/SamlController.php
@@ -17,8 +17,8 @@ class Maestrano_Sso_SamlController extends Mage_Core_Controller_Front_Action
     public function ConsumeAction()
     {
         try {
-            //echo "Consume action!<br/>";
             $resp = new Maestrano_Saml_Response($_POST['SAMLResponse']);
+            Mage::log("## Maestrano_Sso_SamlController->ConsumeAction - is response valid: " . $resp->isValid());
 
             // Check if the Saml response is valid
             if ($resp->isValid()) {
@@ -27,7 +27,9 @@ class Maestrano_Sso_SamlController extends Mage_Core_Controller_Front_Action
 
                 // Create a new maestrano session and save it in magento session
                 $mnoSession = new Maestrano_Sso_Session($_SESSION, $mnoUser);
-                Mage::getSingleton('core/session')->setMnoSession($mnoSession);
+                Mage::getSingleton('admin/session')->setMnoSession($mnoSession);
+
+                Mage::log("## Maestrano_Sso_SamlController->ConsumeAction - Saved session (UID): " . $mnoSession->getUid());
 
                 // Find user in db by uid or email
                 $userModel = Mage::getModel('admin/user');
@@ -37,8 +39,10 @@ class Maestrano_Sso_SamlController extends Mage_Core_Controller_Front_Action
                 // May be a better way to do it...
                 $userObject = Mage::getModel('admin/user')->loadByUsername($userDB['username']);
 
+                Mage::log("## Maestrano_Sso_SamlController->ConsumeAction - User from db: " . $userObject->getUsername());
+
                 // Load the user in the magento session
-                $session = Mage::getSingleton('core/session');
+                $session = Mage::getSingleton('admin/session');
                 $session->setIsFirstVisit(true);
                 $session->setUser($userObject);
                 $session->setAcl(Mage::getResourceModel('admin/acl')->loadAcl());
@@ -47,7 +51,9 @@ class Maestrano_Sso_SamlController extends Mage_Core_Controller_Front_Action
 
                 // If logged in redirect to admin dashboard startup page
                 if ($session->isLoggedIn()) {
+                    Mage::log("## Maestrano_Sso_SamlController->ConsumeAction - User is logged in!");
                     echo $redirectUrl = Mage::getSingleton('adminhtml/url')->getUrl(Mage::getModel('admin/user')->getStartupPageUrl(), array('_current' => false));
+                    Mage::log("## Maestrano_Sso_SamlController->ConsumeAction - Redirecting to: " . $redirectUrl);
                     header('Location: ' . $redirectUrl);
                     exit;
                 }
@@ -56,6 +62,7 @@ class Maestrano_Sso_SamlController extends Mage_Core_Controller_Front_Action
                 echo '<p>Please try again. If issue persists please contact support@maestrano.com<p>';
             }
         } catch (Exception $ex) {
+            Mage::logException($ex);
             echo $ex;
         }
     }
