0957e650c4f81416b0b62d18c0a950fb3c5dc4c9
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2015-07-01 13:43:07 +1000

    Adding connec_mnoidmap setup

--- app/code/local/Maestrano/Connec/sql/connec_setup/install-0.1.0.php
+++ app/code/local/Maestrano/Connec/sql/connec_setup/install-0.1.0.php
@@ -3,18 +3,19 @@ $installer = $this;
 $installer->startSetup();
 
 $sql=<<<SQLTEXT
-CREATE TABLE IF NOT EXISTS `connec_mnoidmap` (
-  `mno_entity_guid` varchar(255) NOT NULL,
-  `mno_entity_name` varchar(255) NOT NULL,
-  `app_entity_id` varchar(255) NOT NULL,
-  `app_entity_name` varchar(255) NOT NULL,
-  `db_timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_flag` int(1) NOT NULL DEFAULT '0'
+create TABLE "connec_mnoidmap" (
+    "mnoidmap_id" int(11) unsigned not null,
+    "mno_entity_guid" varchar(255) not null,
+    "mno_entity_name" varchar(255) not null,
+    "app_entity_id" varchar(255) not null,
+    "app_entity_name" varchar(255) not null,
+    "db_timestamp" timestamp not null default '0000-00-00 00:00:00',
+    "deleted_flag" int(1) not null default '0',
+    PRIMARY KEY ("mnoidmap_id")
 );
-
-CREATE UNIQUE INDEX mno_id_map_unique_key ON `connec_mnoidmap` (`mno_entity_guid`, `mno_entity_name`, `app_entity_id`, `app_entity_name`);
-CREATE INDEX mno_id_map_mno_key ON `connec_mnoidmap` (`mno_entity_guid`, `mno_entity_name`);
-CREATE INDEX mno_id_map_app_key ON `connec_mnoidmap` (`app_entity_id`, `app_entity_name`);
+CREATE UNIQUE INDEX "mno_id_map_unique_key" ON "connec_mnoidmap" ("mno_entity_guid", "mno_entity_name", "app_entity_id", "app_entity_name");
+CREATE INDEX mno_id_map_mno_key ON "connec_mnoidmap" ("mno_entity_guid", "mno_entity_name");
+CREATE INDEX mno_id_map_app_key ON "connec_mnoidmap" ("app_entity_id", "app_entity_name");
 SQLTEXT;
 
 $installer->run($sql);
