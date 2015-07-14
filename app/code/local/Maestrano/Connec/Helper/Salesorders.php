bdda9f96ad072319664cf3ac0cbe5d42a3c3fb03
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2015-07-14 18:21:01 +1000

    Invoices observer and tax calculations

--- app/code/local/Maestrano/Connec/Helper/Salesorders.php
+++ app/code/local/Maestrano/Connec/Helper/Salesorders.php
@@ -25,7 +25,7 @@ class Maestrano_Connec_Helper_Salesorders extends Maestrano_Connec_Helper_BaseMa
     // Map the Connec resource attributes onto the Magento model
     protected function mapConnecResourceToModel($order_hash, &$order)
     {
-
+        // Not saved locally, one way to connec!
     }
 
     // Map the Magento model to a Connec resource hash
@@ -47,39 +47,43 @@ class Maestrano_Connec_Helper_Salesorders extends Maestrano_Connec_Helper_BaseMa
         $order_hash['transaction_date'] = $order->getCreatedAtDate()->toString(Zend_Date::ISO_8601);
         $order_hash['title'] = 'Magento order #' . $order->getIncrementId() . " (" . $order->getCustomerFirstname() . " " . $order->getCustomerLastname() .  ")";
         $order_hash['person_id'] = $customerMnoIdMap['mno_entity_guid'];
-        $order_hash['status'] = 'PAID';
+
+        // State
+        $order_hash['status'] = strtoupper($order->getStatus());
 
         $items = $order->getAllItems();
         if (count($items) > 0) {
             $order_hash['lines'] = array();
             foreach($items as $item) {
-                // Get product mno_id_map
-                $productMnoIdMap = $mnoIdMapModel->findMnoIdMapByLocalIdAndEntityName($item->getProductId(), Mage::helper('mnomap/products')->getLocalResourceName());
-
-                // Item is known
-                if ($productMnoIdMap) {
-                    if ($item->getParentItem()) {
-                        // If there is a parent item, it contains sales infos
-                        $item = $item->getParentItem();
-                    }
-
-                    $line_hash = array();
-
-                    $line_hash['item_id'] = $productMnoIdMap['mno_entity_guid'];
-                    $line_hash['description'] = $item->getName();
-                    $line_hash['quantity'] = $item->getQtyOrdered();
-                    $line_hash['unit_price'] = array();
-                    $line_hash['unit_price']['total_amount'] = $item->getRowTotal();
-                    $line_hash['unit_price']['net_amount'] = $item->getBasePrice();
-                    $line_hash['unit_price']['tax_amount'] = $item->getTaxAmount();
-                    $line_hash['unit_price']['tax_rate'] = $item->getTaxPercent();
-
-                    $order_hash['lines'][] = $line_hash;
+                // If a product is a configured, line are doubled (configured and simple)
+                // We only keep the configured product and interogate db to get simple product id
+                if ($item->getParentItem()) {
+                    continue;
                 }
+
+                // Configurable and simple product both have the simple product sku
+                $product = Mage::getModel('catalog/product')->loadByAttribute('sku', $item->getSku());
+                // Get product mno_id_map
+                $productMnoIdMap = $mnoIdMapModel->findMnoIdMapByLocalIdAndEntityName($product->getId(), Mage::helper('mnomap/products')->getLocalResourceName());
+
+                $line_hash = array();
+
+                $line_hash['item_id'] = $productMnoIdMap['mno_entity_guid'];
+                $line_hash['description'] = $item->getName();
+                $line_hash['quantity'] = $item->getQtyOrdered();
+                $line_hash['unit_price'] = array();
+                $line_hash['unit_price']['total_amount'] = $item->getBasePriceInclTax();
+                $line_hash['unit_price']['tax_rate'] = $item->getTaxPercent();
+                $line_hash['total_price'] = array();
+                $line_hash['total_price']['total_amount'] = $item->getRowTotalInclTax();
+                $line_hash['total_price']['tax_rate'] = $item->getTaxPercent();
+                $line_hash['total_price']['tax_amount'] = $item->getTaxAmount();
+
+                $order_hash['lines'][] = $line_hash;
             }
         }
 
-        Mage::log("Maestrano_Connec_Helper_Salesorders::mapModelToConnecResource - mapped $order_hash: " . print_r($order_hash, 1));
+        Mage::log("Maestrano_Connec_Helper_Salesorders::mapModelToConnecResource - mapped order_hash: " . print_r($order_hash, 1));
 
         return $order_hash;
     }
