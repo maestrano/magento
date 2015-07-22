7ed538bddad63e04297d4d686d255c192c1d3c63
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-07-22 20:32:35 +1000

    Revert patch SQL script to include database name

--- app/code/local/Maestrano/Sso/sql/sso_setup/install-0.1.0.php
+++ app/code/local/Maestrano/Sso/sql/sso_setup/install-0.1.0.php
@@ -3,9 +3,9 @@ $installer = $this;
 $installer->startSetup();
 
 $sql=<<<SQLTEXT
-ALTER TABLE admin_user ADD CONSTRAINT UNQ_EMAIL UNIQUE (email);
-ALTER TABLE admin_user ADD mno_uid VARCHAR(10) NULL;
-ALTER TABLE admin_user ADD CONSTRAINT UNQ_MNO_UID UNIQUE (mno_uid);
+ALTER TABLE magentodb.admin_user ADD CONSTRAINT UNQ_EMAIL UNIQUE (email);
+ALTER TABLE magentodb.admin_user ADD mno_uid VARCHAR(10) NULL;
+ALTER TABLE magentodb.admin_user ADD CONSTRAINT UNQ_MNO_UID UNIQUE (mno_uid);
 SQLTEXT;
 
 $installer->run($sql);
